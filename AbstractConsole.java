96c5d44e16cf47e2aff2bb5875eb6bcc1fa973de
AshleyFarrell85991 <59105476+AshleyFarrell85991@users.noreply.github.com>
2020-02-25 08:34:16 -0500

    Delete AbstractConsole.java

--- AbstractConsole.java
+++
@@ -1,9 +0,0 @@
-import java.io.PrintWriter;
-import java.io.Reader;
-
-public abstract class AbstractConsole {
-	public abstract AbstractConsole printf(String fmt, Object... params);
-	public abstract String readLine();
-	public abstract char[] readPassword();
-	public abstract void flush();
-}
